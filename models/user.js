123b7f5042da8f24759887b50a42f90b08563d2b
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2020-03-30 21:55:44 +0530

    Inital commit

---
+++ models/user.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1")
+
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name:{
+        type:String,
+        requried:true,
+        maxlength:32,
+        trim: true
+    },
+    lastname:{
+        type:String,
+        maxlength:32,
+        trim: true
+    },
+    email:{
+        type:String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    userinfo:{
+        type:String,
+        trim:true
+    },
+    usr_passcod:{
+        type:String,
+        required:true
+    },
+    salt:String,
+    role:{
+        type: Number,
+        default:0
+    },
+    
+    purchases:{
+        type:Array,
+        default: []
+    }
+
+},{timestamps: true});
+
+userSchema.virtual("password")
+    .set(function (password){
+        this._passowrd = password
+        this.salt = uuidv1()
+        this.usr_passcod = this.securePassword(password)
+    })
+    .get(function(){return this._passowrd})
+
+userSchema.method = {
+    securePassword: function(password){
+        if(!passowrd) return "";
+
+        try{
+            return crypto.createHmac('sha256',this.salt)
+            .update(password)
+            .digest('hex')
+        }catch(err)
+        {
+            return err;
+        }
+    },
+    authenticate :  function(password){
+        return this.securePassword(password) === this.usr_passcod
+    }
+}
+
+
+module.exports = mongoose.model("ADMUSRMST",userSchema)
