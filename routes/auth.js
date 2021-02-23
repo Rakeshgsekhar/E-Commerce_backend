32338cd37659c539edea43541a88223222e89abd
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2021-02-23 07:32:25 +0530

    added sigin service

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+/**  validation at router, for valitating input request body */
+const {check} = require("express-validator");
 
-const {signedOut,signUp} = require('../controllers/auth');
+
+const {signedOut,signUp,signIn} = require('../controllers/auth');
 
 router.get("/signout",signedOut);
 
-router.post("/signup",signUp);
+router.post("/signup",[
+check("name").isLength({min:3})
+.withMessage("name should be more than 3 char"),
+check("email").isEmail().withMessage("Please provide a valid email id"),
+check("password")
+.isLength({min:5})
+.withMessage('must be at least 5 char long')
+.matches(/\d/)
+.withMessage('must contain a Number')],
+signUp);
+
+router.post("/signin",[
+check("email").isEmail().withMessage("Please provide an email id"),
+check("password")
+.isLength({min:5})
+.withMessage('password Requried')
+],
+signIn);
 
 module.exports = router;
