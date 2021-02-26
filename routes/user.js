6a67aa0189505e36c7b0b75d1718641a4b3e61f4
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2021-02-26 08:33:40 +0530

    purchase list added

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router(); 
 
-const { getUserById, getUser, getAllUsers, updateUser } = require("../controllers/user");
+const { getUserById, getUser, getAllUsers, updateUser, getUserOrderList } = require("../controllers/user");
 const { isAuthenticated, isSignedIn, isAdmin } = require("../controllers/auth");
 
 
@@ -9,5 +9,6 @@ router.param("id",getUserById);
 router.get("/userid/:id", isSignedIn, isAuthenticated, getUser);
 router.get("/users", getAllUsers);
 router.post("/update/:id",isSignedIn, isAuthenticated, updateUser);
+router.get("/orders/:id", isSignedIn, isAuthenticated, getUserOrderList);
 
 module.exports = router;
